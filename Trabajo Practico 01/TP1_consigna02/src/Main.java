06a5eead5ee19ac6f818fa104033bdb383e174aa
AgusGiorlando <agus.giorlando@gmail.com>
2018-03-29 17:08:55 -0300

    Subiendo TP1-Consigna02

---
+++ Trabajo Practico 01/TP1_consigna02/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Fecha fech1 = new Fecha();
+        fech1.input();
+        fech1.show();
+        System.out.println(fech1.toString());
+
+        Fecha fech2 = new Fecha();
+        fech2.input();
+        fech2.show();
+        System.out.println(fech1.toString());
+
+        System.out.println("---------------------------------------------------------------------");
+        System.out.println("Comparacion: " + fech1.comparar(fech2));
+        System.out.println("---------------------------------------------------------------------");
+
+    }
+}

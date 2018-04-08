3a05d84312647ce73619093c9ebf46e8c71ac503
AgusGiorlando <agus.giorlando@gmail.com>
2018-04-08 00:23:02 -0300

    TP2-Subiendo

---
+++ Trabajo Practico 02/TP2_consigna01/src/main/java/ar/um/edu/programacion_2018/TP2_consigna01/Materia.java
@@ -0,0 +1,40 @@
+package ar.um.edu.programacion_2018.TP2_consigna01;
+
+public class Materia {
+    private String codigoMateria;
+    private String nombre;
+
+    public Materia() {
+        this.codigoMateria = "A01";
+        this.nombre = "Programacion II";
+    }
+
+    public Materia(String codigoMateria, String nombre) {
+        this.codigoMateria = codigoMateria;
+        this.nombre = nombre;
+    }
+
+    public String getCodigoMateria() {
+        return codigoMateria;
+    }
+
+    public void setCodigoMateria(String codigoMateria) {
+        this.codigoMateria = codigoMateria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public static String toString() {
+        return "ar.um.edu.programacion_2018.TP2_consigna01.Materia{" +
+                "codigoMateria='" + codigoMateria + '\'' +
+                ", nombre='" + nombre + '\'' +
+                '}';
+    }
+}
---
+++ Trabajo Practico 02/TP2_consigna01/src/test/java/MateriaTest.java
@@ -0,0 +1,10 @@
+import ar.um.edu.programacion_2018.TP2_consigna01.Materia;
+import org.junit.Test;
+import static org.junit.Assert.assertArrayEquals;
+
+public class MateriaTest {
+    @Test
+    public void toStringTest() {
+        String resultado = Materia.toString();
+    }
+}
